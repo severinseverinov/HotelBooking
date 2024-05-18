4c70d0adb1721900f724558f168c09e21ad7e53a
EkremK015 <ekrembuyukgulen98@gmail.com>
2024-05-18 03:37:00 +0300

    Fetch Update Settings

---
+++ src/features/settings/useSettings.js
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { getSettings } from "../../services/apiSettings";
+
+export function useSettings() {
+      const {isPending, error, data: settings} = useQuery({
+            queryKey: ["settings"],
+                  queryFn: getSettings, 
+      });
+      return {isPending, error, settings};
+}
---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+
+
+
+export function useUpdateSettings() {
+
+
+
+const queryClient = useQueryClient();
+
+
+
+
+const { mutate: updateSetting,  isPending: isUpdating } = useMutation({
+  mutationFn: updateSettingApi, 
+  onSuccess: () => {
+    toast.success("Setting successfully edited");
+    queryClient.invalidateQueries({ queryKey: ["settings"] });
+  },
+  onError: err => toast.error(err.message),
+});
+
+return {isUpdating, updateSetting };
+
+}
